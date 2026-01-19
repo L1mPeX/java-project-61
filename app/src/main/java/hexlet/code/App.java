ee130e72d00c3d3cd3f6e0b59bceee700f238dcc
L1mPeX <andreikazarin@yandex.ru>
2026-01-19 23:37:42 +0300

    add Even game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -19,12 +19,18 @@ public final class App {
     public static void main(final String[] args) {
         System.out.print("""
                 Please enter the game number and press Enter.
+                2 - Even
                 1 - Greet
                 0 - Exit
                 Your choice: """);
         try (Scanner scanner = new Scanner(System.in)) {
             String choice = scanner.nextLine();
             switch (choice) {
+                case "2" -> {
+                    IsEven game = new IsEven();
+                    game.greet();
+                    game.playGame();
+                }
                 case "1" -> Cli.greet();
                 case "0" -> System.out.println("Goodbye!");
                 default -> System.out.println("Unknown choice: " + choice);
